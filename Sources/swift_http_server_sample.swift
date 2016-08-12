9a74b2d9d9f2dc0b304360ca23e2c10a98b1cd32
yagihiro <yagihiro@gmail.com>
2016-08-13 00:10:59 +0900

    fix

--- Sources/swift_http_server_sample.swift
+++ Sources/swift_http_server_sample.swift
@@ -1,6 +1,23 @@
-import Curassow
-import Inquiline
+import HTTP
+import HTTPMiddleware
 
-server { request in
-  return Response(.Ok, contentType: "text/plain", body: "Hello World")
+struct Middleware: HTTPRequestMiddlewareType {
+    func respond(request: HTTPRequest) -> HTTPRequestMiddlewareResult {
+        // You can change the request and pass it forward
+        return .Next(request)
+
+        // Or you can respond early and bypass the chain
+        return .Respond(HTTPResponse(statusCode: 404, reasonPhrase: "Not Found"))
+    }
 }
+
+struct Responder: HTTPResponderType {
+    func respond(request: HTTPRequest) -> HTTPResponse {
+        // May or may not be called depending on the middleware
+        return HTTPResponse(statusCode: 200, reasonPhrase: "OK")
+    }
+}
+
+let request = HTTPRequest(method: .GET, uri: URI(path: "/"))
+let chain = Middleware() >>> Responder()
+let response = chain.respond(request)
