13f8a36c633a2f5dbba07d018c9165811d5d848c
yagihiro <yagihiro@gmail.com>
2016-08-14 03:57:02 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-http-server-sample",
-    dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTPMiddleware.git", majorVersion: 0, minor: 1)
-    ]
+    name: "swift-http-server-sample"
 )
