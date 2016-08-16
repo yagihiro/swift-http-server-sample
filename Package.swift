feb2167e3657c013275de3878db32482a2ea5f63
yagihiro <yagihiro@gmail.com>
2016-08-16 17:11:36 +0900

    tcpip

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-http-server-sample"
+    name: "swift-http-server-sample",
+    dependencies: [
+      .Package(url: "https://github.com/Zewo/TCPIP.git", majorVersion: 0, minor: 4)
+    ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,13 @@
 import Foundation
 
 print("Hello, world")
+
+do {
+  let ip = try IP(port: 5555)
+  let serverSocket = try TCPServerSocket(ip: ip)
+  let clientSocket = try serverSocket.accept()
+
+  let yo = try clientSocket.receiveString(untilDelimiter: "\n")
+} catch {
+
+}
